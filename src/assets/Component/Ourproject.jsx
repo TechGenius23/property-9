8a6de56209ad33c60f526363fb0075aeb21b3e2b
MD: Anwarul Islam Sheam <anwarul.islamsheam999@gmail.com>
2024-11-03 11:54:05 +0600

    ok

--- src/assets/Component/Ourproject.jsx
+++ src/assets/Component/Ourproject.jsx
@@ -2,33 +2,33 @@
 import { NavLink, } from "react-router-dom";
 
 
-const Ourproject = ({projectdata={}}) => {
-   
-    const {estate_title,segment_name,image,price,status,area,location,facilities,description } = projectdata;
+const Ourproject = ({ projectdata = {} }) => {
+
+    const { estate_title, segment_name, image, price, status, area, location, facilities, description } = projectdata;
     return (
         <div>
-            <NavLink to={'/ourproject'}></NavLink>
-            <div className="card bg-base-100 w-96 shadow-xl">
-            
-                <figure>
-                    <img
-                        src={image}
-                        alt="Shoes" />
-                </figure>
-                <div className="card-body">
-                    <h2 className="card-title">{estate_title}</h2>
-                    <p>{segment_name}</p>
-                    <p>{price}</p>
-                    <p>{status}</p>
-                    <p>{area}</p>
-                    <p>{location}</p>
-                    <p>{facilities}</p>
-                    <p>{description}</p>
-                    
+            <NavLink to={'/ourproject'}>
+                <div className="card bg-base-100 w-96 shadow-xl">
+
+                    <figure>
+                        <img
+                            src={image}
+                             />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title">{estate_title}</h2>
+                        <p>{segment_name}</p>
+                        <p>{price}</p>
+                        <p>{status}</p>
+                        <p>{area}</p>
+                        <p>{location}</p>
+                        <p>{facilities}</p>
+                        <p>{description}</p>
+
+                    </div>
+
                 </div>
-                
-            </div>
-            
+            </NavLink>
         </div>
     );
 };
--- src/assets/Component/Review.jsx
+++ src/assets/Component/Review.jsx
@@ -3,20 +3,20 @@ import { NavLink } from "react-router-dom";
 
 
 const Review = (sheam) => {
-    const{reviewerName,jobTitle}=sheam;
+  const { reviewerName, jobTitle } = sheam;
 
-    return (
-          
-            <div className="card bg-base-100 w-96 shadow-xl">
-                <NavLink to={'/review'}></NavLink>
-            <div className="card-body">
-              <h2 className="card-title">{reviewerName}</h2>
-              <h2 className="card-title">{jobTitle}</h2>
-              {/* <p>If a dog chews shoes whose shoes does he choose?</p> */}
-            </div>
-           
-          </div>
-    );
+  return (
+
+    <div className="card bg-base-100 w-96 shadow-xl">
+      <NavLink to={'/review'}></NavLink>
+      <div className="card-body">
+        <h2 className="card-title">{reviewerName}</h2>
+        <h2 className="card-title">{jobTitle}</h2>
+        <p>Are you ready to buy?</p>
+      </div>
+
+    </div>
+  );
 };
 
 export default Review;
